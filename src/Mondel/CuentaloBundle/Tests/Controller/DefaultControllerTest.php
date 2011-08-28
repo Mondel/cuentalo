c3ef86b34af3a4e6ba6bb8435f9cfa7018e5732f
n1co88 <nico.racing.99@gmail.com>
2011-08-28 03:32:59 -0300

    primer test

--- src/Mondel/CuentaloBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Mondel/CuentaloBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,17 +1,19 @@
 <?php
-
 namespace Mondel\CuentaloBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Status 200 en portada");
+        
+        $this->assertTrue($crawler->filter('h3')->count() == 3);
+        $this->assertTrue($crawler->filter('a')->count() == 31);
     }
+    
 }
