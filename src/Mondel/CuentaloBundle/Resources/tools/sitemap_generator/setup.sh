e5d1150b51df70667dfce5e6c519f81f0a9903f8
nicolas <nico.racing.99@gmail.com>
2011-12-17 14:33:35 -0200

    sitemap_generator v0.1

---
+++ src/Mondel/CuentaloBundle/Resources/tools/sitemap_generator/setup.sh
@@ -0,0 +1,1 @@
+cd /home/cuentalo/cuentalo.com.uy/src/Mondel/CuentaloBundle/Resources/tools/sitemap_generator; wget --spider --recursive --no-verbose --output-file=wgetlog.txt http://www.cuentalo.com.uy; sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" wgetlog.txt | sed "s@&@\&amp;@" > sedlog.txt; python sitemap_generator.py
