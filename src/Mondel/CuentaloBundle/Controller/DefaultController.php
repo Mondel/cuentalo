73f4772231435826503189838f01234b09d26ecf
nicolas <nico.racing.99@gmail.com>
2011-12-16 22:47:27 -0200

    pagina y formulario de contacto

--- src/Mondel/CuentaloBundle/Controller/DefaultController.php
+++ src/Mondel/CuentaloBundle/Controller/DefaultController.php
@@ -59,4 +59,52 @@ class DefaultController extends Controller
     	return $this->render(
     			'MondelCuentaloBundle:Paginas:' . $pagina . '.html.twig');
     }
+
+	public function contactoAction()
+	{	
+		$peticion = $this->getRequest();
+		  
+		$form = $this->createFormBuilder()
+			->add('nombre', 'text')
+			->add('email', 'email')
+			->add('mensaje', 'textarea')
+			->add('tipo', 'choice', array(
+					'choices'   => array('0' => 'Consulta', '1' => 'Reportar Error', '2' => 'Otro'),
+					'required'  => true,
+			))
+			->getForm();
+		
+		if ($peticion->getMethod() == 'POST') {
+			$form->bindRequest($peticion);
+		
+			if ($form->isValid()) {
+				
+				$form_data = $form->getData();
+				
+				$tipos = array('0' => 'Consulta', '1' => 'Reportar Error', '2' => 'Otro');
+				$email_to = array('0' => 'info@cuentalo.com.uy', '1' => 'bugs@cuentalo.com.uy', '2' => 'info@cuentalo.com.uy');
+				
+				$form_data_enviar = "Nombre: " . $form_data['nombre'] . "\n"
+					. "Email: " . $form_data['email'] . "\n"
+					. "Tipo: " . $tipos[$form_data['tipo']] . "\n"
+					. "Mensaje: " . $form_data['mensaje'];
+				
+				$mensaje = \Swift_Message::newInstance()
+					->setSubject('Cuentalo: email de activación')
+					->setFrom('registros@cuentalo.com.uy')
+					->setTo('nico.racing.99@gmail.com')
+					->setBody('hola ' . $form_data_enviar);
+				
+				$this->get('mailer')->send($mensaje);
+		
+				$this->get('session')->setFlash('notice', 'Se ha enviado su mensaje. Gracias');
+				return $this->redirect($this->generateUrl('_inicio'));
+			}
+		}
+		
+		return $this->render(
+				'MondelCuentaloBundle:Default:contacto.html.twig',
+				array('form' => $form->createView())
+		);
+	}
 }
