57b034b88d7997a1f0c873d855f6d644d2133568
nicolas <nico.racing.99@gmail.com>
2011-12-27 15:05:13 -0200

    arreglado script analytics

--- src/Mondel/CuentaloBundle/Resources/public/js/layout.js
+++ src/Mondel/CuentaloBundle/Resources/public/js/layout.js
@@ -19,7 +19,7 @@ $(function() {
 });
 
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-26941179-2']);
+_gaq.push(['_setAccount', 'UA-27947390-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
@@ -56,13 +56,3 @@ window.fbAsyncInit = function() {
 	e.src = document.location.protocol + '//connect.facebook.net/es_LA/all.js';
 	document.getElementById('fb-root').appendChild(e);
 }());
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-27779927-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
