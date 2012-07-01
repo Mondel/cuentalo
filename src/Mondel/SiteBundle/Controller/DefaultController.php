6fb648ed891d999a4e1d3373d3b99be27e1e3e99
nicolas <nico.racing.99@gmail.com>
2012-07-01 02:22:21 -0300

    template for homepage in SiteBundle

--- src/Mondel/SiteBundle/Controller/DefaultController.php
+++ src/Mondel/SiteBundle/Controller/DefaultController.php
@@ -2,14 +2,47 @@
 
 namespace Mondel\SiteBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+	Mondel\CuentaloBundle\Entity\Contenido,
+	Mondel\CuentaloBundle\Entity\Comentario,
+	Mondel\CuentaloBundle\Form\Type\ContenidoType,
+	Mondel\CuentaloBundle\Form\Type\ComentarioType;
 
 class DefaultController extends Controller
 {
-    public function showStaticPageAction($page)
+    public function staticPageAction($page)
     {
     	$translate = array('ayuda' => 'help');
 
         return $this->render('MondelSiteBundle:Default:' . $translate[$page] . '.html.twig');
+    }
+
+    public function homePageAction()
+    {
+        $request    = $this->getRequest();
+        $session    = $request->getSession();
+        $em         = $this->getDoctrine()->getEntityManager();
+        $posts      = $em->getRepository('MondelCuentaloBundle:Contenido')->findBy(
+        	array('activo' => '1'),
+        	array('fecha_creacion' => 'DESC'),
+        	5
+    	);
+        
+        $commentsForms = array();
+        foreach ($posts as $post) {
+        	$comment = new Comentario();
+            $commentForm = $this->createForm(new ComentarioType(), $comment);
+            $commentsForms[$post->getId()] = $commentForm->createView();
+        }
+
+        $post     = new Contenido();
+        $postForm = $this->createForm(new ContenidoType(), $post);
+
+        return $this->render('MondelSiteBundle:Default:home_page.html.twig', array(
+        	'posts'         => $posts,
+            'form'          => $postForm->createView(),
+            'commentsForms' => $commentsForms,
+        	'cid'		    => '0'
+    	));
     }
 }
